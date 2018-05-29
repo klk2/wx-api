5f039436dec6f551ddbd76e80a4c83fc032d18c8
klk2 <tianlele_xl@163.com>
2018-05-29 15:29:14 +0800

    udpate

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const app = express();
 
 app.get('/', function (req, res) {
     res.send('Hello World');
-})
+});
 
 const server = app.listen(8081, function () {
     const host = server.address().address;
     const port = server.address().port;
     console.log(" http://%s:%s", host, port)
-
-})
+});
